cc8bbf58985bff322eaaf6d29fb56feb8a8fc483
Armando Bibay Jr <armando.bibayjr@sykes.com>
2020-12-21 21:22:00 +0800

    background notification implemented

--- app/src/main/java/com/raywenderlich/android/drinkit/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/android/drinkit/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.iid.FirebaseInstanceId
 import kotlinx.android.synthetic.main.activity_main.*
@@ -24,6 +26,10 @@ class MainActivity : AppCompatActivity() {
 
     // TODO: create OnClickListener for the button_retrieve_token
       button_retrieve_token.setOnClickListener {
+//          You get the instance of the app that’s tied to the Firebase back end and add a complete listener to it so you know when the task finishes, whether with an error or success.
+//          You check if the task, which is the result of this function, isn’t successful and return an error to the console.
+//          If the task is successful, you get the result from it.
+//          After getting the token, you set it as a string and output it for both the terminal and a toast message.
           // 1
           FirebaseInstanceId.getInstance().instanceId
                   .addOnCompleteListener(OnCompleteListener { task ->
@@ -40,6 +46,13 @@ class MainActivity : AppCompatActivity() {
                       Log.d(TAG, msg)
                       Toast.makeText(baseContext, msg, Toast.LENGTH_LONG).show()
                   })
+
+          if (checkGooglePlayServices()) {
+
+          } else {
+              //You won't be able to send notifications to this device
+              Log.w(TAG, "Device doesn't have google play services")
+          }
       }
 
 
@@ -67,4 +80,23 @@ class MainActivity : AppCompatActivity() {
   companion object {
     private const val TAG = "MainActivity"
   }
+
+//    First, you use Android’s availability API to check for Google Play Services.
+//    If the status isn’t successful, you need to manage the error. In this case, you know the user can’t receive push notifications.
+//    If it is successful, there’s no problem and you can continue with the notification process.
+
+    private fun checkGooglePlayServices(): Boolean {
+        // 1
+        val status = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this)
+        // 2
+        return if (status != ConnectionResult.SUCCESS) {
+            Log.e(TAG, "Error")
+            // ask user to update google play services and manage the error.
+            false
+        } else {
+            // 3
+            Log.i(TAG, "Google play services updated")
+            true
+        }
+    }
 }
